f86fd9d40f72435979b8ead306d056ad6cbf5f28
洪扬 <hongyang@interheart.cn>
2019-06-17 15:41:23 +0800

    修改解码

--- app/src/main/java/com/novel/cn/view/readpage/NetPageLoader.java
+++ app/src/main/java/com/novel/cn/view/readpage/NetPageLoader.java
@@ -8,6 +8,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
@@ -53,39 +54,84 @@ public class NetPageLoader extends PageLoader {
             return null;
         }
         Reader reader = new FileReader(file);
-
+//
         String encoding = ((FileReader) reader).getEncoding();
-        LogUtils.warnInfo("===============encoding");
-        return new BufferedReader(reader);
+        LogUtils.warnInfo(getFilecharset(file) + "===============encoding");
+
+        InputStreamReader isr = new InputStreamReader(new FileInputStream(file), getFilecharset(file));
+        BufferedReader read = new BufferedReader(isr);
+        return new BufferedReader(read);
     }
 
     /**
      * 判断文件的编码格式
-     * @param fileName :file
+     * <p>
+     * //     * @param fileName :file
+     *
      * @return 文件编码格式
      * @throws Exception
      */
-    public static String codeString(String fileName) throws Exception{
-        BufferedInputStream bin = new BufferedInputStream(
-                new FileInputStream(fileName));
-        int p = (bin.read() << 8) + bin.read();
-        String code = null;
-
-        switch (p) {
-            case 0xefbb:
-                code = "UTF-8";
-                break;
-            case 0xfffe:
-                code = "Unicode";
-                break;
-            case 0xfeff:
-                code = "UTF-16BE";
-                break;
-            default:
-                code = "GBK";
+    //判断编码格式方法
+    private static  String getFilecharset(File sourceFile) {
+        String charset = "GBK";
+        byte[] first3Bytes = new byte[3];
+        try {
+            boolean checked = false;
+            BufferedInputStream bis = new BufferedInputStream(new FileInputStream(sourceFile));
+            bis.mark(0);
+            int read = bis.read(first3Bytes, 0, 3);
+            if (read == -1) {
+                return charset; //文件编码为 ANSI
+            } else if (first3Bytes[0] == (byte) 0xFF
+                    && first3Bytes[1] == (byte) 0xFE) {
+                charset = "UTF-16LE"; //文件编码为 Unicode
+                checked = true;
+            } else if (first3Bytes[0] == (byte) 0xFE
+                    && first3Bytes[1] == (byte) 0xFF) {
+                charset = "UTF-16BE"; //文件编码为 Unicode big endian
+                checked = true;
+            } else if (first3Bytes[0] == (byte) 0xEF
+                    && first3Bytes[1] == (byte) 0xBB
+                    && first3Bytes[2] == (byte) 0xBF) {
+                charset = "UTF-8"; //文件编码为 UTF-8
+                checked = true;
+            }
+            bis.reset();
+            if (!checked) {
+                int loc = 0;
+                while ((read = bis.read()) != -1) {
+                    loc++;
+                    if (read >= 0xF0)
+                        break;
+                    if (0x80 <= read && read <= 0xBF) // 单独出现BF以下的，也算是GBK
+                        break;
+                    if (0xC0 <= read && read <= 0xDF) {
+                        read = bis.read();
+                        if (0x80 <= read && read <= 0xBF) // 双字节 (0xC0 - 0xDF)
+                            // (0x80
+                            // - 0xBF),也可能在GB编码内
+                            continue;
+                        else
+                            break;
+                    } else if (0xE0 <= read && read <= 0xEF) {// 也有可能出错，但是几率较小
+                        read = bis.read();
+                        if (0x80 <= read && read <= 0xBF) {
+                            read = bis.read();
+                            if (0x80 <= read && read <= 0xBF) {
+                                charset = "UTF-8";
+                                break;
+                            } else
+                                break;
+                        } else
+                            break;
+                    }
+                }
+            }
+            bis.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        return code;
+        return charset;
     }
 
 
