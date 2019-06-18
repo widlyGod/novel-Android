93240760954ff72586466fe08e77429bf98e21e2
洪扬 <hongyang@interheart.cn>
2019-06-18 18:21:09 +0800

    修改个人信息

--- app/src/main/java/com/novel/cn/mvp/ui/dialog/ModifyNamePopup.kt
+++ app/src/main/java/com/novel/cn/mvp/ui/dialog/ModifyNamePopup.kt
@@ -2,10 +2,14 @@ package com.novel.cn.mvp.ui.dialog
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.text.InputFilter
+import android.text.Selection
+import android.text.Spanned
 import android.view.Gravity
 import android.view.View
 import android.view.animation.Animation
 import com.jakewharton.rxbinding3.view.clicks
+import com.jakewharton.rxbinding3.widget.textChanges
 import com.novel.cn.R
 import com.novel.cn.ext.toast
 import com.novel.cn.mvp.contract.UserInfoContract
@@ -35,6 +39,18 @@ class ModifyNamePopup(context: Context, private val userInfoContractView: UserIn
 
     @SuppressLint("CheckResult")
     private fun bindView() {
+        var maxLen = 20
+        contentView.ed_input.filters = arrayOf(object : InputFilter.LengthFilter(20) {})
+        contentView.ed_input.textChanges().subscribe {
+            var editable = contentView.ed_input.text
+            var len = editable.length
+
+            if (len >= maxLen) {
+                toast("昵称不能超过20字")
+                //设置新光标所在的位置
+                Selection.setSelection(editable, editable.length)
+            }
+        }
         contentView.btn_send.clicks().throttleFirst(1, TimeUnit.SECONDS).subscribe {
             if (contentView.ed_input.text.isBlank()) {
                 toast("昵称不能为空！")
--- app/src/main/java/com/novel/cn/mvp/ui/dialog/ModifySignaturePopup.kt
+++ app/src/main/java/com/novel/cn/mvp/ui/dialog/ModifySignaturePopup.kt
@@ -2,10 +2,13 @@ package com.novel.cn.mvp.ui.dialog
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.text.InputFilter
+import android.text.Selection
 import android.view.Gravity
 import android.view.View
 import android.view.animation.Animation
 import com.jakewharton.rxbinding3.view.clicks
+import com.jakewharton.rxbinding3.widget.textChanges
 import com.novel.cn.R
 import com.novel.cn.ext.toast
 import com.novel.cn.mvp.contract.UserInfoContract
@@ -35,6 +38,17 @@ class ModifySignaturePopup(context: Context, private val userInfoContractView: U
 
     @SuppressLint("CheckResult")
     private fun bindView() {
+        contentView.ed_input.filters = arrayOf(object : InputFilter.LengthFilter(200) {})
+        contentView.ed_input.textChanges().subscribe {
+            var editable = contentView.ed_input.text
+            var len = editable.length
+
+            if (len >= 200) {
+                toast("个性签名不能超过200字")
+                //设置新光标所在的位置
+                Selection.setSelection(editable, editable.length)
+            }
+        }
         contentView.btn_send.clicks().throttleFirst(1, TimeUnit.SECONDS).subscribe {
             if (contentView.ed_input.text.isBlank()) {
                 toast("个性签名不能为空！")
